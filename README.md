af1ab0f168c908035c362682fee642b8e294612b
yangyiyuan5 <yangyiyuan5@jd.com>
2021-12-13 17:55:45 +0800

    feat(index): 完成上传图片

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+**参考文档**
+
+[koa-body](http://www.ptbird.cn/koa-body.html)
--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const router = require('koa-router')();
 const cors = require('koa2-cors');
 // 导入controller middleware:
 const controller = require('./controller');
-const bodyParser = require('koa-bodyparser');
+const koaBody = require('koa-body');
+const path = require("path")
 
 const app = new Koa();
 
@@ -18,10 +19,10 @@ app.use(async (ctx, next) => {
 app.use(cors({
     origin: function (ctx) {
         return '*'
-       /*  if (ctx.url === '/test') {
-            return "*"; // 允许来自所有域名请求
-        }
-        return 'http://localhost:8080'; */
+        /*  if (ctx.url === '/test') {
+             return "*"; // 允许来自所有域名请求
+         }
+         return 'http://localhost:8080'; */
     },
     exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
     maxAge: 5,
@@ -30,7 +31,22 @@ app.use(cors({
     allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
 }))
 
-app.use(bodyParser());
+/* app.use(koaBody({
+    multipart: true, // 支持文件上传
+    encoding: 'gzip',
+    formidable: {
+        uploadDir: path.join(__dirname, 'public/images/'), // 设置文件上传目录
+        keepExtensions: true,    // 保持文件的后缀
+        maxFieldsSize: 2 * 1024 * 1024, // 文件上传大小
+        onFileBegin: (name, file) => { // 文件上传前的设置
+            console.log(`name: ${name}`);
+            console.log(file);
+        },
+    }
+})); */
+app.use(koaBody({
+    multipart: true,
+}));
 // 使用middleware:
 app.use(controller());
 // add router middleware:
